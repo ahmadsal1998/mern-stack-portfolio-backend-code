7bd092d680c5c282bf45c09ec01c0e8056fbe077
ahmad abu salame <78186457+ahmadsal1998@users.noreply.github.com>
2024-05-29 19:15:06 +0300

    test env

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: ['https://ahmad-salame-portfolio.netlify.app', 'https://my-portfolioo-dashboard.netlify.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
